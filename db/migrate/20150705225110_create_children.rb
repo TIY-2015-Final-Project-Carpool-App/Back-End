21a5115dc18ad0322137f2a842fbfb8757a2d6fc
Henry Phan <hphan.cs@gmail.com>
2015-07-05 18:54:29 -0400

    Add Child model and controller, removed incorrectly named user controller

---
+++ db/migrate/20150705225110_create_children.rb
@@ -0,0 +1,16 @@
+class CreateChildren < ActiveRecord::Migration
+  def change
+    create_table :children do |t|
+      t.string :first_name
+      t.string :last_name
+      t.integer :age
+      t.date :dob
+      t.string :address
+      t.string :phone_number
+      t.intger :height
+      t.integer :weight
+
+      t.timestamps null: false
+    end
+  end
+end
