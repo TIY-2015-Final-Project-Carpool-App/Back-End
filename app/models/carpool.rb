089da0507f4b0845f63c8901ff5e6af6c614822f
Henry Phan <hphan.cs@gmail.com>
2015-07-16 00:55:45 -0400

    Add carpool_id to appointment and added carpool associations

--- app/models/carpool.rb
+++ app/models/carpool.rb
@@ -4,4 +4,5 @@ class Carpool < ActiveRecord::Base
   has_many :joined_carpools, dependent: :destroy
   has_many :users, through: :joined_carpools
   belongs_to :creator, class_name: "User", foreign_key: :creator_id
+  has_many :appointments, dependent: :destroy
 end
