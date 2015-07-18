25ceb4e93ead5020d3b479191c1c04c09f55244f
Henry Phan <hphan.cs@gmail.com>
2015-07-18 19:03:43 -0400

    Fixed bug for appointment model, change before_validation to after_save

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -5,7 +5,7 @@ class Appointment < ActiveRecord::Base
   belongs_to :creator, class_name: "User", foreign_key: :creator_id
   has_many :riders, dependent: :destroy
 
-  after_validation :set_coordinates
+  after_save :set_coordinates
 
   def set_coordinates
     origin_coord = Geocoder.coordinates(self.origin)
