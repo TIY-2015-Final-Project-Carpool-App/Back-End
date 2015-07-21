7a579c882778408a74b461daf53fefcc64062914
Henry Phan <hphan.cs@gmail.com>
2015-07-21 13:25:55 -0400

    Modify resque.rb

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -2,6 +2,8 @@
 # Resque.after_fork = Proc.new { ActiveRecord::Base.establish_connection }
 
 uri = URI.parse(ENV["REDISTOGO_URL"])
-Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :thread_safe => true)
+# Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :thread_safe => true)
+
+Resque.redis = Redis.new(url: uri)
 
 # Dir["/carpoolbackend/app/jobs/*.rb"].each { |file| require file }
